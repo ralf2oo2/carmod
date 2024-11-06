466dc5fbaed0115e583d036b212ace6d3463551c
Ralf2oo2 <ralf2oo2@hotmail.com>
2024-11-06 17:03:52 +0100

    World collision wip

--- src/main/java/ralf2oo2/carmod/PhysicsEngine.java
+++ src/main/java/ralf2oo2/carmod/PhysicsEngine.java
@@ -1,6 +1,10 @@
 package ralf2oo2.carmod;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.modificationstation.stationapi.api.tick.TickScheduler;
+import net.modificationstation.stationapi.api.util.math.StationBlockPos;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.util.vector.Vector3f;
@@ -14,16 +18,19 @@ import ralf2oo2.carmod.vehicle.VehicleModel;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Stream;
 
 public class PhysicsEngine implements Runnable{
     private static final Logger logger = LogManager.getLogger(PhysicsEngine.class);
     private static final int MAX_CONTACTS = 10;
+    private static final int BOUNDING_PADDING = 1;
     private static final DVector3C yunit = new DVector3(0, 1, 0);
     private static final DVector3C zunit = new DVector3(0, 0, 1);
     private final DWorld world;
     private final DSpace space;
     private final DJointGroup contactGroup;
     private final Map<CarEntity, DBody[]> collisionBodies = new HashMap<>();
+    private final Map<BlockPos, DGeom> worldCollision = new HashMap<>();
     private final List<CarEntity> removalQueue = new ArrayList<>();
     public final Queue<Runnable> executionQueue = new ConcurrentLinkedQueue<>();
 
@@ -34,7 +41,7 @@ public class PhysicsEngine implements Runnable{
         contactGroup = OdeHelper.createJointGroup();
         space = OdeHelper.createSimpleSpace();
         world.setContactSurfaceLayer (0.001);
-        OdeHelper.createPlane(space, 0, 1, 0, 64);
+        //OdeHelper.createPlane(space, 0, 1, 0, 64);
     }
 
     public Optional<DBody[]> getEntityBodies(CarEntity entity){
@@ -135,6 +142,7 @@ public class PhysicsEngine implements Runnable{
             pollQueue();
             updateEntities();
             removeDeadEntities();
+            updateWorldCollision();
         }
         catch (Exception e){
             logger.error("Exception in physics thread: {}", e.getMessage(), e);
@@ -174,6 +182,40 @@ public class PhysicsEngine implements Runnable{
         }
     }
 
+    private void updateWorldCollision(){
+        Minecraft minecraft = CarmodClient.getMc();
+        for (Map.Entry<CarEntity, DBody[]> entry : collisionBodies.entrySet()) {
+            CarEntity carEntity = entry.getKey();
+            if(carEntity.prevX == carEntity.x) return;
+            Vehicle vehicle = VehicleRegistry.getVehicle(carEntity.carName).get();
+            RenderwareBinaryStream.ColBounds bounds = vehicle.vehicleCollisions.collisions.header().bounds();
+            float width = bounds.max().y() - bounds.min().y();
+            float height = bounds.max().z() - bounds.min().z();
+            //BlockPos entityPos = new BlockPos((int)Math.floor(carEntity.x), (int)Math.floor(carEntity.y), (int)Math.floor(carEntity.z));
+            Stream<BlockPos> blockStream = StationBlockPos.stream(Box.create(carEntity.x - (width  * 0.5 + BOUNDING_PADDING),
+                                            carEntity.y - (height  * 0.5 + BOUNDING_PADDING),
+                                            carEntity.z - (width  * 0.5 + BOUNDING_PADDING),
+                                            carEntity.x + (width  * 0.5 + BOUNDING_PADDING),
+                                            carEntity.y + (height  * 0.5 + BOUNDING_PADDING),
+                                            carEntity.z + (width  * 0.5 + BOUNDING_PADDING)
+            ));
+
+            blockStream.forEach(blockPos -> {
+                if(!worldCollision.containsKey(blockPos.toImmutable())){
+                    int id = minecraft.world.getBlockId(blockPos.x, blockPos.y, blockPos.z);
+                    if(id > 0) {
+                        DGeom block = OdeHelper.createBox(space, 1, 1, 1);
+                        block.setPosition((double) blockPos.x + 0.5, (double) blockPos.y, (double) blockPos.z + 0.5);
+                        worldCollision.put(blockPos.toImmutable(), block);
+                    }
+                }
+            });
+
+//            TickScheduler.CLIENT_RENDER_START.immediate(() -> {
+//            });
+        }
+    }
+
     private void removeDeadEntities(){
         for(CarEntity entity : removalQueue){
             DBody body = collisionBodies.get(entity)[0];
@@ -188,8 +230,10 @@ public class PhysicsEngine implements Runnable{
         int i,n;
         final int N = 10;
 
-        if (o1 == null || o2 == null || OdeHelper.areConnected(o1.getBody(), o2.getBody())) {
-            return; // Skip collision if bodies are connected by any joint
+        if(o1.getBody() != null && o2.getBody() != null){
+            if (o1 == null || o2 == null || OdeHelper.areConnected(o1.getBody(), o2.getBody())) {
+                return; // Skip collision if bodies are connected by any joint
+            }
         }
 
         DContactBuffer contacts = new DContactBuffer(N);
